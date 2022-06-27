2c5db5a320efb23caeb797c9a87f41215a989e95
pmodhe19 <pmodhe19@gmail.com>
2022-06-27 12:53:37 +0530

    seperate file for class removed

--- C++/Fraction Class/program.cpp
+++ C++/Fraction Class/program.cpp
@@ -1,7 +1,59 @@
 #include<iostream>
 using namespace std;
-#include "fractionClass.cpp"
+
+class Fraction{
+    private:
+      int numerator,denominator;
+    public:
+       Fraction(int numerator,int denominator){
+          this->numerator=numerator;
+          this->denominator=denominator;
+       }
+
+
+       void print(){
+           int gcd=1;
+           int j=min(this->numerator,this->denominator);
+           for(int i=1;i<=j;i++){
+              if((this->numerator%i==0) && (this->denominator%i==0)){
+                  gcd=i;
+              }
+           }
+           cout<<(this->numerator)/gcd<<" / "<<(this->denominator)/gcd<<endl;
+       }
+
+       
+
+       void add(Fraction f2){
+          int lcm=denominator * f2.denominator;
+          int x=lcm/denominator;
+          int y=lcm/f2.denominator;
+          int num = x*numerator + y*f2.numerator;
+          numerator=num;
+          denominator=lcm;
+       }       
+       
+       void simplify(){
+           int gcd=1;
+           int j=min(this->numerator,this->denominator);
+           for(int i=1;i<=j;i++){
+              if((this->numerator%i==0) && (this->denominator%i==0)){
+                  gcd=i;
+              }
+           }
+           this->numerator=numerator/gcd;
+           this->denominator=denominator/gcd;
+       }
+       
+
+    
+};
 
 int main(){
     Fraction f1(10,2);
+    Fraction f2(15,4);
+    f1.add(f2);
+    f1.print();
+    //f1.simplify();
+    f2.print();
 }
