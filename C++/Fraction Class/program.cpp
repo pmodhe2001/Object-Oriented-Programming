033a1aa77a77b4c18b050f769e22ed4676e38dd0
pmodhe19 <pmodhe19@gmail.com>
2022-06-27 18:08:35 +0530

    Implemented seperate simplify function

--- C++/Fraction Class/program.cpp
+++ C++/Fraction Class/program.cpp
@@ -12,26 +12,8 @@ class Fraction{
 
 
        void print(){
-           int gcd=1;
-           int j=min(this->numerator,this->denominator);
-           for(int i=1;i<=j;i++){
-              if((this->numerator%i==0) && (this->denominator%i==0)){
-                  gcd=i;
-              }
-           }
-           cout<<(this->numerator)/gcd<<" / "<<(this->denominator)/gcd<<endl;
+           cout<<(this->numerator)<<" / "<<(this->denominator)<<endl;
        }
-
-       
-
-       void add(Fraction f2){
-          int lcm=denominator * f2.denominator;
-          int x=lcm/denominator;
-          int y=lcm/f2.denominator;
-          int num = x*numerator + y*f2.numerator;
-          numerator=num;
-          denominator=lcm;
-       }       
        
        void simplify(){
            int gcd=1;
@@ -44,9 +26,17 @@ class Fraction{
            this->numerator=numerator/gcd;
            this->denominator=denominator/gcd;
        }
-       
 
-    
+       void add(Fraction f2){
+          int lcm=denominator * f2.denominator;
+          int x=lcm/denominator;
+          int y=lcm/f2.denominator;
+          int num = x*numerator + y*f2.numerator;
+          numerator=num;
+          denominator=lcm;
+          simplify();
+       }       
+       
 };
 
 int main(){
