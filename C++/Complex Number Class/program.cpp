f92e88b3718d4b7bca9c792a7824029071d347c1
pmodhe19 <pmodhe19@gmail.com>
2022-06-27 19:09:31 +0530

    Complex No class completed with Addition and Multiplication member function

--- C++/Complex Number Class/program.cpp
+++ C++/Complex Number Class/program.cpp
@@ -9,13 +9,32 @@ class ComplexNo{
              this->real=real;
              this->imaginary=imaginary;
           }
+
+          void print(){
+            cout<<this->real<<" "<<this->imaginary<<"i"<<endl;
+          }
+
+          void add(ComplexNo c2){
+            this->real=this->real + c2.real;
+            this->imaginary=this->imaginary + c2.imaginary;
+          }
+
+          void multiply(ComplexNo c2){
+            this->real=this->real * c2.real;
+            this->imaginary=this->imaginary * c2.imaginary;
+          }
 };
 
 int main(){
+    //ComplexNo c1,c2;
     int real1,imaginary1;
     cin>>real1>>imaginary1;
     int real2,imaginary2;
     cin>>real2>>imaginary2;
-    ComplexNo(real1,imaginary1);
-    ComplexNo(real2,imaginary2);
+    ComplexNo c1=ComplexNo(real1,imaginary1);
+    ComplexNo c2=ComplexNo(real2,imaginary2);
+    c1.add(c2);
+    c1.print();
+    c1.multiply(c2);
+    c1.print();
 }
